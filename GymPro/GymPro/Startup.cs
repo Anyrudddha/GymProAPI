03cf68bf2e6ab3f193e4266d9cd3acab0ac66177
VitthalPawar22324 <59561742+VitthalPawar22324@users.noreply.github.com>
2020-01-22 13:55:53 +0530

    FinalCommit

--- GymPro/GymPro/Startup.cs
+++ GymPro/GymPro/Startup.cs
@@ -19,23 +19,12 @@ namespace GymPro
         {
             Configuration = configuration;
         }
-        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
-            services.AddCors(options =>
-            {
-                options.AddPolicy(MyAllowSpecificOrigins,
-                builder =>
-                {
-                    builder.WithOrigins("https://localhost:44338").AllowAnyMethod().AllowAnyOrigin();
-                });
-            });
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -50,7 +39,7 @@ namespace GymPro
             {
                 app.UseHsts();
             }
-            app.UseCors(MyAllowSpecificOrigins);
+
             app.UseHttpsRedirection();
             app.UseMvc();
         }
