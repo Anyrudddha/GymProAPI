a70cd7bc5a2507f47081636decf938fb1c8fe11c
vs-amar <59562003+vs-amar@users.noreply.github.com>
2020-01-22 11:51:00 +0530

    New Api Added

---
+++ GymPro/GymPro/Models/GymOwners.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymPro.Models
+{
+    public class GymOwners
+    {
+
+        public string Gym_Id { get; set; }
+        public string Gym_Name { get; set; }
+        public string Gym_Owner_Name { get; set; }
+        public string Gym_Address { get; set; }
+        public string Gym_Pincode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gym_Website { get; set; }
+        public string Gym_MailId { get; set; }
+        public string Gym_Registration_Date { get; set; }
+        public string Password { get; set; }
+        public string Gym_Certification_Number { get; set; }
+        public string Gym_Location_longitude { get; set; }
+        public string Gym_Location_latitude { get; set; }
+        public string Company_Registration_Date { get; set; }
+        public string Service_Tax_Number { get; set; }
+        public string GST_Number { get; set; }
+        public string status { get; set; }
+    }
+}
